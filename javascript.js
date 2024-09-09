c26857d1e475f248c806c795322dab16cf0cf97e
Pere <awusapere@gmail.com>
2024-09-09 10:28:56 +0100

    initial commit

---
+++ javascript.js
@@ -0,0 +1,79 @@
+let t = "John";
+let surname = "Doe";
+
+console.log(t);
+console.log(surname);
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+// const pi = 3.14;
+// pi = 10;
+
+// console.log(pi);
+
+/*exercises*/
+let a = 23;
+let b = 97;
+
+console.log(23+97)
+
+let c=24;
+let d=4;
+let e=6;
+let f=7;
+let g=8;
+let h=9;
+
+console.log(24+4+6+7+8+9)
+
+let result = (4 + 6 + 9) / 77;
+console.log(result);
+
+let y = 10;
+
+console.log(9*y)
+
+let z =7*y;
+console.log(z)
+
+const max = 57;
+const actual= max - 13;
+const percentage= actual / max;
+
+console.log(percentage)
+
+
+/*STRINGS*/
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const badString = string;
+console.log(badString);
+
+const single = 'Single quotes';
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
+
+const name = "Chris";
+const greeting = `Hello, ${name}`;
+console.log(greeting); // "Hello, Chris"
+
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined); // "Hello, how are you?"
+
+
+
+
+
+
